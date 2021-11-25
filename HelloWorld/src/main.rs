96d7f6dba615830cf8a852331b60e88506a7a6d5
my81tw <my81tw@gmail.com>
2021-11-25 23:03:15 +0800

    2nd commit

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -1,3 +1,23 @@
+struct Fruit {
+    color:i32
+}
+impl Fruit {
+    fn get(&self) -> i32 {
+        self.color
+    }
+    fn set(&mut self,  value: &mut i32) {
+        println!("set *value={}, value={}", *value, value);
+        if *value>10 {
+            *value = 10;
+        }
+        self.color = *value;
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut array: [i32; 3] = [0; 3];
+    let mut f = Fruit{color:3};
+    let mut i :i32 = 899;
+    f.set(&mut i);
+    // array[4]=10;
+    println!("Hello, world i = {}, f={}!", i, f.get());
 }
